8650a11b7f8b6bf01c96664b7e28d0ec0c4675b0
Laurence Emms <laurenceemms@gmail.com>
2016-07-04 00:19:12 -0700

    Added Freetype.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char**argv)
 {
     std::cout << "Testing sfml\n";
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    std::cout << "Loading FreeType\n";
+    FT_Library library;
+    auto error = FT_Init_FreeType(&library);
+    if (error)
+    {
+        std::cerr << "Failed to load Freetype library.\n";
+        return -1;
+    }
+    sf::RenderWindow window(sf::VideoMode(470, 242, sf::Style::Fullscreen), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
